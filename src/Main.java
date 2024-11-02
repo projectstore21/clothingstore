ed7e9304e7bdff9a4141861c7fb7480f544356fc
nadezhdachurilo <nadezhdachurilo@gmail.com>
2024-11-02 20:42:27 +0100

    В классе Main создайте экземпляры класса Товар #10

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to our clothing store!");
+
+
+
+
+
+        Product product1 = new Product("Men's shirt", 7.88, 5);
+        Product product2 = new Product("Men's jacket", 45, 8);
+        Product product3 = new Product("Men's pants", 15, 4);
+        Product product4 = new Product();
+        product4.setProductName("Men's socks");
+        product4.setPrice(3);
+        product4.setAmountInStock(30);
+        System.out.println(product1);
+        System.out.println(product2);
+        System.out.println(product3);
+        System.out.println(product4);
+
+
+
+
+
+
+
     }
 }
