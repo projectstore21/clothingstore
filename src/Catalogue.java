0252c3579818c6a3e752edeca0422cffcae82e97
KatherineP3101 <kpanova2k@gmail.com>
2024-11-04 18:35:51 +0100

    Внесла изменения по комментам Юли

--- src/Catalogue.java
+++ src/Catalogue.java
@@ -6,59 +6,46 @@ public class Catalogue {
     private static int uniqueCatalogueId = 0;
     private final int catalogueId;
     private String catalogueName;
-    private ArrayList<Product> CatalogueProducts;
+    private ArrayList<Product> catalogueProducts;
 
-    // Создаем пустой конструктор:
     public Catalogue() {
         this.catalogueId = ++uniqueCatalogueId;
     }
 
-    // Создаем не пустой конструктор:
     public Catalogue(int catalogueId, String catalogueName, ArrayList<Product> CatalogueProducts) {
         this.catalogueId = ++uniqueCatalogueId;
         this.catalogueName = catalogueName;
-        this.CatalogueProducts = CatalogueProducts;
+        this.catalogueProducts = CatalogueProducts;
     }
 
-    // Сеттер ID каталога
-    public void setCatalogueId (String CatalogueId) {
-        this.catalogueId = catalogueId;
-    }
-
-    // Сеттер наименования каталога (раздела)
     public void setCatalogueName (String CatalogueName) {
         this.catalogueName = catalogueName;
     }
 
-    // Метод для добавления товар(ов) в каталог
     public void addProductToCatalogue (Product product) throws ProductNotFoundException {
         if (product != null) {
-            this.CatalogueProducts.add(product);
+            this.catalogueProducts.add(product);
         } else {
             throw new ProductNotFoundException("Product not found.");
         }
     }
 
-    // Геттер ID каталога
     public int getCatalogueId () {
         return catalogueId;
     }
 
-    // Геттер наименования каталога (раздела)
     public String getCatalogueName () {
         return catalogueName;
     }
 
-    // Геттер списка товаров в каталоге
     public ArrayList<Product> getCatalogueProductList () {
-        return CatalogueProducts;
+        return catalogueProducts;
     }
 
-    // Переопределение метода toString()
     @Override
     public String toString() {
         return "Catalogue Id: " + catalogueId +
                 "; catalogue name: " + catalogueName +
-                "; catalogue products: " + CatalogueProducts + ".";
+                "; catalogue products: " + catalogueProducts + ".";
     }
 }
