f700b9db1431aa92f4c7aa1c01bb6eed91e65bf4
nadezhdachurilo <nadezhdachurilo@gmail.com>
2024-11-04 20:25:32 +0100

    Добавила ProductId

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to our clothing store!");
 
-
-
-
-
         Product product1 = new Product("Men's shirt", 7.88, 5);
         Product product2 = new Product("Men's jacket", 45, 8);
         Product product3 = new Product("Men's pants", 15, 4);
@@ -13,16 +9,18 @@ public class Main {
         product4.setProductName("Men's socks");
         product4.setPrice(3);
         product4.setAmountInStock(30);
-        System.out.println(product1);
-        System.out.println(product2);
-        System.out.println(product3);
-        System.out.println(product4);
-
-
-
-
-
-
-
+        Product product5 = new Product("Men's T-shirt", 7, 32);
+        Product product6 = new Product("Men's Sweater", 35, 8);
+        Product product7 = new Product("Men's Hoodie", 23, 12);
+        Product product8 = new Product("Men's Scarf", 18, 4);
+        Product product9 = new Product("Men's Underwear", 5, 35);
+        Product product10 = new Product("Men's Blazer", 15, 13);
+        Product product11 = new Product("Men's Suit", 34, 8);
+        Product product12 = new Product("Men's Shorts", 14, 2);
+        Product product13 = new Product("Men's Chinos", 15, 9);
+        Product product14 = new Product("Men's Sportswear", 57, 4);
+        Product product15 = new Product("Men's Tie", 13, 4);
     }
 }
+
+
